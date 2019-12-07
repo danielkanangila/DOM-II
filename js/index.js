c31c57efd242317fc04c4334082b91ab41245c2d
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-12-06 19:26:51 -0500

    Added mouseover effect on nav items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+// Add mouseover effect on nav items
+document.querySelectorAll('nav a')
+    .forEach((item, index) => {
+        let navLinkSpan = document.createElement('span');
+        const id = `navLinkSpan${index}`;
+        navLinkSpan.setAttribute('id', id);
+        item.appendChild(navLinkSpan);
+
+        item.addEventListener('mouseover', () => {
+            navLinkSpan = document.getElementById(id);
+            navLinkSpan.className = 'nav-link-hover-show';
+        })
+        item.addEventListener('mouseout', () => {
+            navLinkSpan = document.getElementById(id);
+            navLinkSpan.className = 'nav-link-hover-hide';
+        })
+    });
+
